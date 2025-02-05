e654e5b8f4a2ff55486982bb8e7668d6d2ede8be
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 22:08:32 +0000

    Update PropertyList.js

--- frontend/src/components/PropertyList.js
+++ frontend/src/components/PropertyList.js
@@ -1,1 +1,33 @@
+// PropertyList.js (Displaying List of Properties)
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import PropertyCard from './PropertyCard';
+
+function PropertyList() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/properties');
+        setProperties(response.data);
+      } catch (error) {
+        console.error('Error fetching properties', error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+  return (
+    <div className="property-list">
+      {properties.map(property => (
+        <PropertyCard key={property._id} property={property} />
+      ))}
+    </div>
+  );
+}
+
+export default PropertyList;
 
