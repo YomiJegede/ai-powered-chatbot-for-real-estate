1bee344b53367459f9e369599e33f32476fb5e35
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 21:37:52 +0000

    Update propertyService.js

--- backend/services/propertyService.js
+++ backend/services/propertyService.js
@@ -1,1 +1,16 @@
 
+
+const Property = require('../models/Property');
+
+// Get all properties
+exports.getAllProperties = async () => {
+  return await Property.find();
+};
+
+// Create a new property
+exports.createProperty = async (propertyData) => {
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+  return newProperty;
+};
+
