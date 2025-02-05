f05baf9525aeea37dd749428337fefd8fd793a24
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 21:39:09 +0000

    Update Property.js

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -1,1 +1,16 @@
 
+const mongoose = require('mongoose');
+
+const PropertySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  bedrooms: { type: Number },
+  imageUrl: { type: String },
+}, { timestamps: true });
+
+const Property = mongoose.model('Property', PropertySchema);
+
+module.exports = Property;
+
