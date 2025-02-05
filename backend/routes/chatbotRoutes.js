fb09f476b60d01869d390f184e1582b68e265e16
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 21:40:37 +0000

    Update chatbotRoutes.js

--- backend/routes/chatbotRoutes.js
+++ backend/routes/chatbotRoutes.js
@@ -1,1 +1,10 @@
 
+
+const express = require('express');
+const router = express.Router();
+const chatbotController = require('../controllers/chatbotController');
+
+router.post('/', chatbotController.handleChatbotRequest);
+
+module.exports = router;
+
