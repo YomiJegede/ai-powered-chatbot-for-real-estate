fb8a157114db5458441d74d295f281e32f5173ae
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 21:41:22 +0000

    Update apiResponse.js

--- backend/utils/apiResponse.js
+++ backend/utils/apiResponse.js
@@ -1,1 +1,10 @@
+# Helper for API responses
+
+exports.success = (res, data, message = "Success") => {
+  return res.status(200).json({ message, data });
+};
+
+exports.error = (res, error, message = "Error") => {
+  return res.status(500).json({ message, error });
+};
 
