7b43f47525424b58c40347ca09f3b0c5c4a7fe85
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 22:13:32 +0000

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,12 +1,21 @@
 // AuthContext.js (User Authentication Context)
 // To manage the authentication state globally, we can use React Context.
 
-import React, { createContext, useState } from 'react';
+ import React, { createContext, useState, useEffect } from 'react';
 
+// Create Context
 export const AuthContext = createContext();
 
+// Create a provider component
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));  // Load user from localStorage if available
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
