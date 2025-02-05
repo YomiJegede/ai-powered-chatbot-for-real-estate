3b4b9af1660ecf9e7b76ef02362367c62124493c
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-02-05 21:40:17 +0000

    Update propertyRoutes.js

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -1,1 +1,12 @@
 
+
+const express = require('express');
+const router = express.Router();
+const propertyController = require('../controllers/propertyController');
+
+router.get('/', propertyController.getAllProperties);
+router.get('/:id', propertyController.getPropertyById);
+router.post('/', propertyController.createProperty);
+
+module.exports = router;
+
